8c565d506aa30816c8e9b3671742e708e7d07bc9
Marcin Dylewski <bounceerr@gmail.com>
2015-11-08 15:32:09 +0100

    Register layout changes.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,18 @@
 module UsersHelper
   def form_with_error_handling(name, &html)
-    content_tag(:div, class: 'form-group') do
+   content_tag(:div, class: 'form-group') do
       content_tag(:div, class: 'col-md-6') do
         res = [] << content_tag(:div, class: "input-group has-feedback #{'has-error' if not @user.errors[name].empty?}") do
           res = []
           res << content_tag(:span, class: 'input-group-addon new-user-group-addon') do
-            name.to_s.split(/_/).map {|t| t.capitalize }.join(" ")
+            if name == :password_confirmation
+              n = :confirm
+            elsif name == :nationality_id
+              n = :nationality
+            else
+              n = name
+            end
+            n.to_s.split(/_/).map {|t| t.capitalize }.join(" ")
           end
           res.append(yield html)
           if not @user.errors[name].empty?
