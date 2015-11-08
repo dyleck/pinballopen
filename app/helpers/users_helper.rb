2922e5cb2e705c182b48be83347eb6521988828d
Marcin Dylewski <bounceerr@gmail.com>
2015-11-08 16:05:14 +0100

    Centered login form and disable margin on error.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,30 +1,31 @@
 module UsersHelper
   def form_with_error_handling(name, &html)
-   content_tag(:div, class: 'form-group') do
-      content_tag(:div, class: 'col-md-6') do
-        res = [] << content_tag(:div, class: "input-group has-feedback #{'has-error' if not @user.errors[name].empty?}") do
-          res = []
-          res << content_tag(:span, class: 'input-group-addon new-user-group-addon') do
-            if name == :password_confirmation
-              n = :confirm
-            elsif name == :nationality_id
-              n = :nationality
-            else
-              n = name
+     error = !@user.errors[name].empty?
+     content_tag(:div, class: "form-group #{'form-group-no-margin-bottom' if error}") do
+        content_tag(:div, class: nil) do
+          res = [] << content_tag(:div, class: "input-group has-feedback #{'has-error' if error }") do
+            res = []
+            res << content_tag(:span, class: 'input-group-addon new-user-group-addon') do
+              if name == :password_confirmation
+                n = :confirm
+              elsif name == :nationality_id
+                n = :nationality
+              else
+                n = name
+              end
+              n.to_s.split(/_/).map {|t| t.capitalize }.join(" ")
             end
-            n.to_s.split(/_/).map {|t| t.capitalize }.join(" ")
+            res.append(yield html)
+            if error
+              res << content_tag(:span, class: 'form-control-feedback glyphicon glyphicon-remove') {}
+            end
+            res.join.html_safe
           end
-          res.append(yield html)
-          if not @user.errors[name].empty?
-            res << content_tag(:span, class: 'form-control-feedback glyphicon glyphicon-remove') {}
+          if error
+            res << content_tag(:strong, @user.errors[name].first, class: 'help-block', style: 'color: red;')
           end
           res.join.html_safe
         end
-        if not @user.errors[name].empty?
-          res << content_tag(:strong, @user.errors[name].first, class: 'help-block', style: 'color: red;')
-        end
-        res.join.html_safe
       end
-    end
   end
 end
