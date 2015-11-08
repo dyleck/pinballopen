a4abd1c11fdea87401d035e42ab703bf58a8b391
Marcin Dylewski <bounceerr@gmail.com>
2015-11-08 15:09:13 +0100

    Added helper for registration form.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+  def form_with_error_handling(name, &html)
+    content_tag(:div, class: 'form-group') do
+      content_tag(:div, class: 'col-md-6') do
+        res = [] << content_tag(:div, class: "input-group has-feedback #{'has-error' if not @user.errors[name].empty?}") do
+          res = []
+          res << content_tag(:span, class: 'input-group-addon new-user-group-addon') do
+            name.to_s.split(/_/).map {|t| t.capitalize }.join(" ")
+          end
+          res.append(yield html)
+          if not @user.errors[name].empty?
+            res << content_tag(:span, class: 'form-control-feedback glyphicon glyphicon-remove') {}
+          end
+          res.join.html_safe
+        end
+        if not @user.errors[name].empty?
+          res << content_tag(:strong, @user.errors[name].first, class: 'help-block')
+        end
+        res.join.html_safe
+      end
+    end
+  end
 end
