7fe9e195be46dd957fb4bae120feb1931e2d7bae
Marcin Dylewski <bounceerr@gmail.com>
2015-11-20 09:54:51 +0100

    Created DoubleKO and Final models.

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,8 +7,12 @@ class TournamentsController < ApplicationController
 
   def new
     @tournament = Tournament.new
-    @tournament.phases << Qualification.new(number_of_rounds: 10, matches_per_round: 1, players_per_round: 1,
-                                            player_fixed: true, phase_fixed: false)
+    @tournament.phases << [ Qualification.new(number_of_rounds: 10, matches_per_round: 1, players_per_round: 1,
+                                            player_fixed: true, phase_fixed: false),
+                            DoubleKO.new(number_of_rounds: 64, matches_per_round: 3, players_per_round: 2,
+                                        player_fixed: false, phase_fixed: true),
+                            Final.new(number_of_rounds: 1, matches_per_round: 3, players_per_round: 3,
+                                      player_fixed: false, phase_fixed: true) ]
   end
 
   def create
---
+++ app/models/double_ko.rb
@@ -0,0 +1,2 @@
+class DoubleKO < Phase
+end
